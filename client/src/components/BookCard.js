cc57641961f603db9d78a8e96994de9d1f033b17
Guillermo Lara <grlara9@gmail.com>
2020-02-03 21:11:13 -0800

    update state books with data from google api and render bookcard with data images, title author and publish data

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-export default function BookCard() {
+export default function BookCard(props) {
     return (
-        <div>
+        <div className="card-container">
+            <img src={props.image} alt="" />
+            <div className="desc">
+    <h2>{props.title}</h2>
+    <h3>{props.author}</h3>
+    <p>{props.published}</p>
+            </div>
             
         </div>
     )
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import BookCard from './BookCard'
 
-export default function BookList() {
-    return (
+export default function BookList(props) {
+    return(
+
         <div className="List">
             {
-                props.books.map((book) =>{
-                    return <BookCard />
+                props.books.map((book, k) =>{
+                    
+                    return <BookCard image={book.volumeInfo.imageLinks.thumbnail} 
+                    key={k}
+                    title={book.volumeInfo.title} author={book.volumeInfo.authors} 
+                    published={book.volumeInfo.publishedDate}/>
                 })
             }
         </div>
-    )
+            )
+    
 }
